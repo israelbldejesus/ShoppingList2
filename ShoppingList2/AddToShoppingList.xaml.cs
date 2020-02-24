0f6d9006bbc7871035c644ec60b5884ef132be8d
IsraelB <israelbldejesus@gmail.com>
2020-02-24 15:24:33 -0600

    first commit

---
+++ ShoppingList2/AddToShoppingList.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList2
+{
+    /// <summary>
+    /// Interaction logic for AddToShoppingList.xaml
+    /// </summary>
+    public partial class AddToShoppingList : UserControl
+    {
+        public AddToShoppingList()
+        {
+            InitializeComponent();
+            AddButton.Click += OnAddButtonClicked;
+        }
+        public void OnAddButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ShoppingListData data) 
+            {
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }//var data = DataContext as ShoppingListData;
+        }
+        
+    }
+}
